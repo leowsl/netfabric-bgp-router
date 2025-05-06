abb890b38eb70ee6e7816e4085f384d183158412
Leonard Wechsler <leonard.wechsler@netfabric.ai>
2025-05-06 11:41:22 +0200

    fix: remove dynamic typing for state trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ pub mod utils;
 pub use utils::thread_manager::ThreadManager;
 pub use utils::message_bus::MessageBus;
 pub use utils::state_machine::StateMachine;
+pub use utils::state_machine::State;
 pub use components::router::Router;
-pub use components::live_bgp_parser; 
+pub use components::live_bgp_parser;
--- src/utils/state_machine.rs
+++ src/utils/state_machine.rs
@@ -2,27 +2,11 @@ use crate::utils::message_bus::{Message, MessageBusError, MessageReceiver, Messa
 use crate::utils::thread_manager::{ThreadManagerError, ThreadManager};
 use log::{info, error};
 use uuid::Uuid;
-use std::thread::{JoinHandle, sleep};
+use std::thread::sleep;
 use std::time::Duration;
-use std::sync::Arc;
 use thiserror::Error;
+use std::marker::PhantomData;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum InternalState {
-    Initialized,
-    Running,
-    Paused,
-    Stopped,
-}
-impl Message for InternalState {}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum StateMachineCommand {
-    Start,
-    Pause,
-    Resume,
-    Stop,
-}
 
 
 #[derive(Error, Debug)]
@@ -33,16 +17,36 @@ pub enum StateMachineError {
     MessageBusError(#[from] MessageBusError),
     #[error("State machine error: {0}")]
     StateMachineError(String),
+    #[error("No Result was computed.")]
+    NoResult(),
 }
 
+pub trait State: Send + Sync + 'static + Sized {
+    fn work(self) -> Option<Self>;
+}
 
-pub struct StateMachine {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum InternalState {
+    Initialized,
+    Running,
+    Paused,
+    Stopped,
+}
+impl Message for InternalState {}
+impl State for InternalState {
+    fn work(self) -> Option<Self> { Some(self) }
+}
+
+pub struct StateMachine<T: State> {
     command_sender: MessageSender,
     runner_thread_id: Uuid,
+    _phantom: PhantomData<T>,   // This is used to avoid the compiler warning about unused type parameters
+    // TODO: Add a result field
+    // result: Result<Box<dyn State>, StateMachineError>,
 }
 
-impl StateMachine {
-    pub fn new(thread_manager: &mut ThreadManager) -> Result<Self, StateMachineError> {
+impl<T: State> StateMachine<T> {
+    pub fn new(thread_manager: &mut ThreadManager, state: T) -> Result<Self, StateMachineError> {
         // Create a message bus channel for the state machine
         let mut message_bus = thread_manager.lock_message_bus()?;
         let channel_id = message_bus.create_channel(1)?;
@@ -51,16 +55,18 @@ impl StateMachine {
         drop(message_bus);
 
         // Create the state machine
-        let state_machine = StateMachine {
+        let state_machine = StateMachine::<T> {
             command_sender: sender,
-            runner_thread_id: thread_manager.start_thread(move || Self::runner(receiver))?,
+            runner_thread_id: thread_manager.start_thread(move || Self::runner(receiver, state))?,
+            _phantom: PhantomData,
         };
 
         Ok(state_machine)
     }
 
-    fn runner(state_machine_command_receiver: MessageReceiver) {
+    fn runner(state_machine_command_receiver: MessageReceiver, initial_state: T) {
         let mut internal_state = InternalState::Initialized;
+        let mut state: Option<T> = Some(initial_state);
 
         // Loop
         while internal_state != InternalState::Stopped {
@@ -77,19 +83,14 @@ impl StateMachine {
             // Execute logic
             match internal_state {
                 InternalState::Running => {
-                    info!("State machine is running");
+                    state = state.unwrap().work();
+                    if state.is_none() {
+                        info!("State machine is done. Stopping...");
+                        internal_state = InternalState::Stopped;
+                    }
                 }
-                InternalState::Initialized | InternalState::Paused => {
-                    info!("State machine is paused");
-                    sleep(Duration::from_secs(1));
-                }
-                InternalState::Stopped => {
-                    info!("Stopping state machine...");
-                }
-                _ => {
-                    error!("State machine is in an invalid state. Stopping...");
-                    internal_state = InternalState::Stopped;
-                }
+                InternalState::Initialized | InternalState::Paused => sleep(Duration::from_secs(1)),
+                InternalState::Stopped => internal_state = InternalState::Stopped,
             }
         }
     }
@@ -130,24 +131,52 @@ mod tests {
     use super::*;
     use crate::utils::thread_manager::ThreadManager;
     
+    struct SampleState {
+        data: i32,
+    }
+
+    impl State for SampleState {
+        fn work(mut self) -> Option<Self> {
+            if self.data > 0 {
+                self.data -= 1;
+            } else {
+                info!("Counter is 0. Resetting to 10");
+                self.data = 10;
+            }
+            Some(self)
+        }
+    }
+
     #[test]
     fn create_state_machine(){
         let mut thread_manager = ThreadManager::new();
-        let state_machine = StateMachine::new(&mut thread_manager);
+        let start_state = SampleState { data: 10 };
+        let state_machine = StateMachine::new(&mut thread_manager, start_state);
         assert!(state_machine.is_ok());
     }
 
     #[test]
     fn start_state_machine() -> Result<(), StateMachineError> {
         let mut thread_manager = ThreadManager::new();
-        let mut state_machine = StateMachine::new(&mut thread_manager)?;
+        let start_state = SampleState { data: 10 };
+        let mut state_machine = StateMachine::new(&mut thread_manager, start_state)?;
         return state_machine.start();
     }
 
     #[test]
     fn stop_state_machine() -> Result<(), StateMachineError> {
         let mut thread_manager = ThreadManager::new();
-        let mut state_machine = StateMachine::new(&mut thread_manager)?;
+        let start_state = SampleState { data: 10 };
+        let mut state_machine = StateMachine::new(&mut thread_manager, start_state)?;
+        state_machine.start()?;
+        return state_machine.stop();
+    }
+
+    #[test]
+    fn test_state_machine_with_samplestate()  -> Result<(), StateMachineError> {
+        let mut thread_manager = ThreadManager::new();
+        let start_state = SampleState { data: 10 };
+        let mut state_machine = StateMachine::new(&mut thread_manager, start_state)?;
         state_machine.start()?;
         return state_machine.stop();
     }
